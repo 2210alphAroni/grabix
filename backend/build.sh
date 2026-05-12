b8a7e80d47745e33c891d294ed76f9f55fcedaa1
Md Nabinur Islam Roni <22103371@iubat.edu>
2026-05-12 21:44:26 +0600

    fix render build script

--- backend/build.sh
+++ backend/build.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-
 npm install
-
 pip install -U yt-dlp
-
-apt-get update
-apt-get install -y ffmpeg python3 python3-pip curl
-
-curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
-apt-get install -y nodejs
